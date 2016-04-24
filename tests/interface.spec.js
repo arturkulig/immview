e0ab96bfd5d887826efccac2c4a1134b1ac04d6c
Artur Kulig <artur.kulig@swmansion.com>
2016-04-25 00:18:22 +0200

    check if all classes fulfill basic Reactor interface

--- tests/interface.spec.js
+++ tests/interface.spec.js
@@ -4,10 +4,31 @@ import {
     Domain,
 } from '../src';
 
-describe('Immview', function () {
-    it('exports', function () {
-        expect(Data).toBeDefined();
-        expect(View).toBeDefined();
-        expect(Domain).toBeDefined();
+function fulfillsReactorInterface(instance) {
+    expect(typeof instance.read).toBe('function');
+    expect(typeof instance.subscribe).toBe('function');
+    expect(typeof instance.appendReactor).toBe('function');
+    expect(typeof instance.map).toBe('function');
+}
+
+describe('Immview', () => {
+    describe('has public interface', () => {
+        it('Data', () => {
+            expect(Data).toBeDefined();
+            const d = new Data();
+            fulfillsReactorInterface(d);
+            expect(typeof d.write).toBe('function');
+        });
+        it('View', () => {
+            expect(View).toBeDefined();
+            const v = new View();
+            fulfillsReactorInterface(v);
+        });
+        it('Domain', () => {
+            expect(Domain).toBeDefined();
+            const testData = new Data();
+            const testDomain = new Domain(testData);
+            fulfillsReactorInterface(testDomain);
+        });
     });
 });
