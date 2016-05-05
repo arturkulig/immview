0a947c6287473cc1734e1d7f4fdcdd60b5949522
Artur Kulig <artur.kulig@swmansion.com>
2016-05-05 09:46:18 +0200

    uglify as plugin not loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: __dirname + '/dist',
@@ -15,16 +16,28 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                loader: 'uglify',
-            },
-            {
-                test: /\.js$/,
                 loader: 'babel',
                 query: {
                     presets: ['es2015'],
+                    plugins: ['transform-object-rest-spread'],
                 },
             },
         ],
     },
     devtool: 'source-map',
+    plugins: [],
 };
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+            pure_getters: true,
+            unsafe: true,
+            unsafe_comps: true,
+            screw_ie8: true,
+            warnings: false
+        }
+    }));
+}
+
+module.exports = config;
