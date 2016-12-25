f6e42b382b6105d1235be8bffc2aa24b92f892c4
Artur Kulig <arturkulig@gmail.com>
2016-12-25 19:39:37 +0100

    Data implementation

--- src/Data.ts
+++ src/Data.ts
@@ -1,5 +1,18 @@
+import { Dispatcher } from './DispatcherInstance'
+import { DispatcherPriorities } from './DispatcherPriorities'
+import { MessageTypes } from './BaseObservable'
 import { Observable } from './Observable'
 
 export class Data<T> extends Observable<T> {
+    constructor(defaultValue: T) {
+        super(() => { })
+        this.lastValue = defaultValue
+    }
 
+    write(nextValue: T) {
+        return new Promise(resolve => {
+            Data.getQueue(this).push([MessageTypes.Next, nextValue, resolve])
+            Data.dispatchDigestMessages()
+        })
+    }
 }
