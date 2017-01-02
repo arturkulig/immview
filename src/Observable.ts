f860f99fec7649f9d0b602169cdc454180a890b5
Artur Kulig <arturkulig@gmail.com>
2017-01-02 16:21:13 +0100

    Observable interface

--- src/Observable.ts
+++ src/Observable.ts
@@ -1,5 +1,19 @@
 import { BaseObservable } from './BaseObservable'
 
 export class Observable<T> extends BaseObservable<T> {
+    map<U> (functor: (value: T) => U): Observable<U> {
+        throw new Error('not implemented')
+    }
 
+    reduce<U> (reductor: (value: T, summary: U) => U): Observable<U> {
+        throw new Error('not implemented')
+    }
+
+    filter (filter: (value: T) => boolean): Observable<T> {
+        throw new Error('not implemented')
+    }
+
+    scan<U> (scanner: (values: T[]) => U, historyLength = 2, defaultValue = null): Observable<U> {
+        throw new Error('not implemented')
+    }
 }
