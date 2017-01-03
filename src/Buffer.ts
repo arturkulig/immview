5723ebde30f7fe1f188ac4f472beaf12cc36f64b
Artur Kulig <arturkulig@gmail.com>
2017-01-03 12:59:23 +0100

    buffering from factory, not class

--- src/Buffer.ts
+++
@@ -1,5 +0,0 @@
-import { Observable } from './Observable'
-
-export class Buffer<T> extends Observable<T> {
-
-}
--- src/Observable.ts
+++ src/Observable.ts
@@ -1,19 +1,23 @@
 import { BaseObservable } from './BaseObservable'
 
 export class Observable<T> extends BaseObservable<T> {
-    map<U> (functor: (value: T) => U): Observable<U> {
+    map<U>(functor: (value: T) => U): Observable<U> {
         throw new Error('not implemented')
     }
 
-    reduce<U> (reductor: (value: T, summary: U) => U): Observable<U> {
+    reduce<U>(reductor: (value: T, summary: U) => U): Observable<U> {
         throw new Error('not implemented')
     }
 
-    filter (filter: (value: T) => boolean): Observable<T> {
+    filter(filter: (value: T) => boolean): Observable<T> {
         throw new Error('not implemented')
     }
 
-    scan<U> (scanner: (values: T[]) => U, historyLength = 2, defaultValue = null): Observable<U> {
+    scan<U>(scanner: (values: T[]) => U, historyLength = 2, defaultValue = null): Observable<U> {
+        throw new Error('not implemented')
+    }
+
+    buffer(maxMessages: number = 0): Observable<T> {
         throw new Error('not implemented')
     }
 }
