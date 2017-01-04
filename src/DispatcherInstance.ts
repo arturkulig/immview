13e669f3b207e552e4bee60b96e02f7ed38ddc18
Artur Kulig <arturkulig@gmail.com>
2017-01-04 18:16:03 +0100

    Domain action decorator implementation

--- src/DispatcherInstance.ts
+++ src/DispatcherInstance.ts
@@ -2,7 +2,7 @@ import { Dispatcher } from './Dispatcher'
 import { DispatcherPriorities } from './DispatcherPriorities'
 
 const DispatcherInstance = new Dispatcher()
-const dispatch = (job: () => any, priority: DispatcherPriorities) => {
+const dispatch = (job: () => any, priority: DispatcherPriorities = DispatcherPriorities.EXTERNAL) => {
     DispatcherInstance.push(job, priority)
     DispatcherInstance.run()
 }
---
+++ src/Domain.action.spec.ts
@@ -0,0 +1,28 @@
+import { Observable } from './Observable'
+import { Domain } from './Domain'
+import { dispatch } from './DispatcherInstance'
+import { action } from './Domain.action'
+
+describe('Domain', () => {
+    it('extending class method can be decorated to become an action', () => {
+        class DerivativeOfDomain<T> extends Domain <{}, {}>{
+            tested = false
+
+            constructor() {
+                super(new Observable(), {})
+            }
+
+            @action
+            test(v: boolean) {
+                this.tested = v
+            }
+        }
+
+        const subject = new DerivativeOfDomain()
+        dispatch(() => {
+            subject.test(true)
+            expect(subject.tested).toBe(false)
+        })
+        expect(subject.tested).toBe(true)
+    })
+})
