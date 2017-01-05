0421afd261e29d80037666b0e002fcad922625ab
Artur Kulig <arturkulig@gmail.com>
2017-01-05 12:01:10 +0100

    Domain.create factory

--- src/DispatcherInstance.ts
+++ src/DispatcherInstance.ts
@@ -6,8 +6,21 @@ const dispatch = (job: () => any, priority: DispatcherPriorities = DispatcherPri
     DispatcherInstance.push(job, priority)
     DispatcherInstance.run()
 }
+const dispatchPromise = (job: () => any, priority: DispatcherPriorities = DispatcherPriorities.EXTERNAL) => {
+    return new Promise<void>((resolve, reject) => {
+        dispatch(() => {
+            try {
+                resolve(job())
+            } catch (e) {
+                console.error(e.stack || e.message || e)
+                reject(e)
+            }
+        }, priority)
+    })
+}
 
 export {
     DispatcherInstance as Dispatcher,
-    dispatch
+    dispatch,
+    dispatchPromise
 }
