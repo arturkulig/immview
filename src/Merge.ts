82a59dc46e971661b5b27dfd79441a88741ba53c
Artur Kulig <arturkulig@gmail.com>
2017-01-06 18:51:02 +0100

    Merge awaits for signals from all sources before releasing first value

--- src/Merge.ts
+++ src/Merge.ts
@@ -2,20 +2,56 @@ import { Observable } from './Observable'
 
 export class Merge<T extends Object> extends Observable<T> {
     constructor(sources: { [id: string]: Observable<any> }) {
-        let push = null
-        const subscriptions = []
-
         super(observer => {
+            const names: string[] = []
+            const initialized: string[] = []
+
+            let preinitialReceivedMessages: [string, any][] = []
+            let result: T = ({} as T)
+
+            const subscriptions = []
             for (let key in sources) {
                 if (!Object.prototype.hasOwnProperty.call(sources, key)) continue
+                names.push(key)
                 subscriptions.push(
-                    sources[key].subscribe(sourceValue => {
-                        observer.next(merged => {
-                            return ({
-                                ...(merged as Object),
-                                [key]: sourceValue
-                            } as T)
-                        })
+                    sources[key].subscribe(nextSourceValue => {
+                        preinitialReceivedMessages && preinitialReceivedMessages.push([key, nextSourceValue])
+                        if (initialized.indexOf(key) < 0) {
+                            initialized.push(key)
+                        }
+
+                        if (names.length !== initialized.length) {
+                            return
+                        }
+
+                        if (preinitialReceivedMessages) {
+                            let initialMessage: T = ({} as T)
+                            let initializedFields: string[] = []
+                            const repeatedPreinitialReceivedMessages: [string, any][] = []
+                            for (let i = 0; i < preinitialReceivedMessages.length; i++) {
+                                if (initializedFields.indexOf(preinitialReceivedMessages[i][0]) < 0) {
+                                    const [sourceName, sourceMessage] = preinitialReceivedMessages[i]
+                                    initialMessage[sourceName] = sourceMessage
+                                    initializedFields.push(sourceName)
+                                } else {
+                                    repeatedPreinitialReceivedMessages.push(preinitialReceivedMessages[i])
+                                }
+                            }
+                            observer.next(result = initialMessage)
+                            repeatedPreinitialReceivedMessages.forEach(([sourceName, sourceMessage]) => {
+                                observer.next(result = ({
+                                    ...(result as {}),
+                                    [sourceName]: sourceMessage
+                                } as T))
+                            })
+                            preinitialReceivedMessages = null
+                            return
+                        }
+
+                        observer.next(result = ({
+                            ...(result as {}),
+                            [key]: nextSourceValue
+                        } as T))
                     })
                 )
             }
