c1f30ab24ffcee7712479317fa14118db5f230dd
Artur Kulig <arturkulig@gmail.com>
2017-01-06 18:51:02 +0100

    Merge awaits for signals from all sources before releasing first value

--- src/Merge.spec.ts
+++ src/Merge.spec.ts
@@ -4,19 +4,35 @@ import { Merge } from './Merge'
 describe('Merge', () => {
     it('can be created with observables', done => {
         const expectedValues = [
-            { o1: 'o1' },
-            { o1: 'o1', o2: 'o2' },
             { o1: 'o1', o2: 'o2', o3: 'o3' },
+            { o1: 'o1b', o2: 'o2', o3: 'o3' },
+            { o1: 'o1b', o2: 'o2b', o3: 'o3' },
+            { o1: 'o1b', o2: 'o2c', o3: 'o3' },
         ]
-        const subject = new Merge<{ o1: string, o2: string, o3: string }>({
-            o1: new Observable(observer => { observer.next('o1') }),
-            o2: new Observable(observer => { observer.next('o2') }),
-            o3: new Observable(observer => { observer.next('o3') }),
+        const lastExpectedValue = expectedValues[expectedValues.length - 1]
+        new Merge<{ o1: string, o2: string, o3: string }>({
+            o1: new Observable(observer => {
+                observer.next('o1')
+                observer.next('o1b')
+            }),
+            o2: new Observable(observer => {
+                observer.next('o2')
+                setTimeout(() => {
+                    observer.next('o2b')
+                    observer.next('o2c')
+                }, 100)
+            }),
+            o3: new Observable(observer => {
+                observer.next('o3')
+            }),
         })
-        subject.subscribe(merged => {
-            expect(merged).toEqual(expectedValues.shift())
-            if (merged.o1 && merged.o2 && merged.o3) setTimeout(done)
-        })
+            .subscribe(merged => {
+                expect(merged).toEqual(expectedValues.shift())
+                try {
+                    expect(merged).toEqual(lastExpectedValue)
+                    setTimeout(done)
+                } catch (e) { }
+            })
     })
     it('listens to changes', done => {
         let push = null
@@ -24,17 +40,17 @@ describe('Merge', () => {
             push = observer.next
             push(1)
         })
-        const subject = new Merge<{ source: number }>({ source })
         const values = []
-        subject.subscribe(merged => {
-            values.push(merged.source)
-            if (
-                values[0] === 1 &&
-                values[1] === 3
-            ) {
-                setTimeout(done)
-            }
-        })
+        new Merge<{ source: number }>({ source })
+            .subscribe(merged => {
+                values.push(merged.source)
+                if (
+                    values[0] === 1 &&
+                    values[1] === 3
+                ) {
+                    setTimeout(done)
+                }
+            })
         setTimeout(() => {
             push(3)
         })
