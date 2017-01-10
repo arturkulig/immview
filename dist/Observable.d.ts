290327259406ea8cdba2c4452da470d2aee92c0b
Artur Kulig <arturkulig@gmail.com>
2017-01-10 11:15:17 +0100

    build

--- dist/Observable.d.ts
+++ dist/Observable.d.ts
@@ -3,9 +3,9 @@ export declare class Observable<T> extends BaseObservable<T> {
     static of<T>(...values: T[]): Observable<T>;
     static from<T>(values: Observable<T> | Iterable<T>): Observable<T>;
     map<U>(action: (value: T) => U): Observable<U>;
-    flatMap<U>(action: (value: T) => Observable<U>): Observable<U>;
-    reduce<U>(reductor: (value: T, summary: U) => U): Observable<U>;
+    flatten<U>(this: Observable<Observable<U>>): Observable<U>;
+    scan<U>(reductor: (value: T, summary: U) => U): Observable<U>;
     filter(filter: (value: T) => boolean): Observable<T>;
-    scan<U>(scanner: (values: T[]) => U, historyLength?: number, defaultValue?: any): Observable<U>;
+    bufferCount(bufferSize: number, customBufferCount?: number): Observable<T[]>;
     buffer(maxLastMessages?: number): Observable<T[]>;
 }
