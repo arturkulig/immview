e3bfe1944aa48df4f59e1159abbb7213ec27a74e
Artur Kulig <arturkulig@gmail.com>
2017-01-30 10:35:36 +0100

    return actions result

--- src/Domain.action.ts
+++ src/Domain.action.ts
@@ -1,11 +1,17 @@
 import { DispatcherPriorities } from './DispatcherPriorities'
 import { dispatchPromise } from './DispatcherInstance'
+import { diagnose } from './Diagnose'
 
 export function action(target, propertyKey: string, descriptor: PropertyDescriptor) {
     const actionPerformer: Function = descriptor.value
     descriptor.value = function (...args) {
         return dispatchPromise(
-            () => { actionPerformer.apply(this, args) },
+            () => {
+                const diagDone = diagnose.measure(`<\$ ${this['name'] ? `${this['name']}.` : ''}${propertyKey}`)
+                const result = actionPerformer.apply(this, args)
+                diagDone && diagDone()
+                return result
+            },
             DispatcherPriorities.DOMAIN
         )
     }
