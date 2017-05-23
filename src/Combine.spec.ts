c5bb0e7d1e09f267eb4f44ca583c199be189a519
Artur Kulig <arturkulig@gmail.com>
2017-05-23 21:52:56 +0200

    Combine closes subscriptions when it closes itself

--- src/Combine.spec.ts
+++ src/Combine.spec.ts
@@ -4,7 +4,7 @@ import { Combine } from './Combine'
 import { flush } from './DispatcherInstance'
 
 describe('Combine', () => {
-    it('can be created with observables', done => {
+    it('can be created with observables', async () => {
         const expectedValues = [
             { o1: null, o2: null, o3: null },
             { o1: 'o1', o2: null, o3: null },
@@ -15,7 +15,7 @@ describe('Combine', () => {
             { o1: 'o1c', o2: 'o2b', o3: 'o3' },
             { o1: 'o1c', o2: 'o2c', o3: 'o3' },
         ]
-        const lastExpectedValue = expectedValues[expectedValues.length - 1]
+        const received = []
 
         const o1 = new Observable<string>()
         const o2 = new Observable<string>()
@@ -23,24 +23,22 @@ describe('Combine', () => {
 
         new Combine<{ o1: string, o2: string, o3: string }>({ o1, o2, o3, })
             .subscribe(merged => {
-                expect(merged).toEqual(expectedValues.shift())
-                if (expectedValues.length === 0) setTimeout(done)
+                received.push(merged)
             })
 
-        flush().then(() => {
-            o1.next('o1')
-            o1.next('o1b')
-            o1.next('o1c')
-            o2.next('o2')
-            setTimeout(() => {
-                o2.next('o2b')
-                o2.next('o2c')
-            }, 100)
-            o3.next('o3')
-        })
+        o1.next('o1')
+        o1.next('o1b')
+        o1.next('o1c')
+        o2.next('o2')
+        o3.next('o3')
+        await new Promise(resolve => setTimeout(resolve))
+        o2.next('o2b')
+        o2.next('o2c')
+
+        expect(received).toEqual(expectedValues)
     })
 
-    it('can be created with atoms', done => {
+    it('can be created with atoms', async () => {
         const expectedValues = [
             { o1: 'start', o2: 'start', o3: 'start' },
             { o1: 'o1', o2: 'start', o3: 'start' },
@@ -50,7 +48,7 @@ describe('Combine', () => {
             { o1: 'o1b', o2: 'o2b', o3: 'o3' },
             { o1: 'o1b', o2: 'o2c', o3: 'o3' },
         ]
-        const lastExpectedValue = expectedValues[expectedValues.length - 1]
+        const received = []
 
         const o1 = new Atom<string>('start')
         const o2 = new Atom<string>('start')
@@ -58,18 +56,18 @@ describe('Combine', () => {
 
         new Combine<{ o1: string, o2: string, o3: string }>({ o1, o2, o3, })
             .subscribe(merged => {
-                expect(merged).toEqual(expectedValues.shift())
-                if (expectedValues.length === 0) setTimeout(done)
+                received.push(merged)
             })
 
         o1.next('o1')
         o1.next('o1b')
         o2.next('o2')
-        setTimeout(() => {
-            o2.next('o2b')
-            o2.next('o2c')
-        }, 100)
         o3.next('o3')
+        await new Promise(resolve => setTimeout(resolve))
+        o2.next('o2b')
+        o2.next('o2c')
+
+        expect(received).toEqual(expectedValues)
     })
 
     it('closes as first of sources closes', () => {
